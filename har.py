9ae9b7c53b94a325f5100df10516d164e3ba4cc5
dulalchakrabarti <dulalchakrabarti@gmail.com>
2024-09-22 22:25:09 +0530

    Add files via upload

---
+++ har.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+fl = open("har.csv","w+")
+fl.write("sn"+","+"st"+","+"dist"+","+"stn"+","+"rf"+"\n")
+url = "http://aws.imd.gov.in:8091/AWS/dataview.php?a=AWSAGRO&b=HARYANA&c=ALL_DISTRICT&d=ALL_STATION&e=2024-09-04&f=2024-09-05&g=ALL_HOUR&h=ALL_MINUTE"
+html = requests.get(url).text
+#print(html)
+soup = BeautifulSoup(html,"lxml")
+table = soup.find('table')
+rows = table.findAll('tr')
+for tr in rows:
+ cols = tr.findAll('td')
+ lst = []
+ for td in cols:
+  lst.append(td.get_text().encode("utf-8"))
+ lst1 = [x.decode("utf-8") for x in lst]
+ if (len(lst1)) > 0:
+  print(lst1[:6])
+  ln = ','.join(x for x in lst1[:6])
+  fl.write(ln+'\n') 
+'''
+stn ={'LODI ROAD':['28.59','77.2221']}
+lat = ''
+lon = ''
+#read lat long file & store in a dictionary
+fl = open("lodi_road.csv","w+")
+fl.write("stn"+","+"lat"+","+"lon"+","+"date"+","+"rf"+","+"tt"+","+"rh"+","+"wd"+","+"ws"+","+"gust"+","+"mslp"+"\n")
+#url = "http://aws.imd.gov.in:8091/AWS/dataview.php?a=AWS&b=DELHI&c=NEW_DELHI&d=LODI ROAD&e=2019-01-01&f=2019-01-31&g=03&h=00"
+url = "http://aws.imd.gov.in:8091/AWS/dataview.php?a=AWS&b=DELHI&c=NEW_DELHI&d=LODI%20ROAD&e=2022-06-01&f=2024-8-15&g=ALL_HOUR&h=ALL_MINUTE"
+html = requests.get(url).text
+soup = BeautifulSoup(html,"lxml")
+table = soup.find('table')
+rows = table.findAll('tr')
+for tr in rows:
+ cols = tr.findAll('td')
+ lst = []
+ for td in cols:
+  lst.append(td.get_text().encode("utf-8"))
+ lst1 = [x.decode("utf-8") for x in lst]
+ if (len(lst1)) > 0:
+  lat = stn[lst1[2]][0]
+  lon = stn[lst1[2]][1]
+  print(lst1[2],lat,lon,lst1[3],lst1[4],lst1[5])
+  fl.write(lst1[2]+','+str(lat)+','+str(lon)+','+str(lst1[3])+' '+str(lst1[4])+','+str(lst1[5])+','+str(lst1[6])+','+str(lst1[9])+','+str(lst1[11])+','+str(lst1[12])+','+str(lst1[13])\
+  +','+str(lst1[15])+'\n')
+''' 
