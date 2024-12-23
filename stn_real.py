efb256373c4d0c1caf3096be30213c1fba920f6d
Dulal Chakrabarti <dulalchakrabarti@gmail.com>
2024-12-23 20:47:26 +0530

    Add files via upload

--- stn_real.py
+++ stn_real.py
@@ -1,4 +1,36 @@
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.ogimet.com/cgi-bin/gsynres?lang=en&ind=42182&ndays=30&ano=2024&mes=07&day=01&hora=03&ord=DIR&Send=Send"
+dfs = pd.read_html(url)
+names = dfs[2].columns
+date = dfs[2][('Date', 'Date')]
+rain = dfs[2][('Prec. (mm)', 'Prec. (mm)')]
+# concatenating the DataFrames 
+df = pd.concat([date, rain], join = 'outer', axis = 1)
+print(df)
+'''
+
+print(dfs[2]['Max'])
+print(dfs[2]['Rain'+'\n'+'(mm)'])
+
+
+df = pd.DataFrame(columns=['Date', 'Rain(mm)'])
+df_
+
+#url = "https://www.ogimet.com/cgi-bin/gsynres?lang=en&ind=42182&ndays=30&ano=2024&mes=07&day=01&hora=03&ord=DIR&Send=Send"
+html = requests.get(url).text
+soup = BeautifulSoup(html,"lxml")
+table = soup.find('table')
+rows = table.findAll('tr')
+for tr in rows:
+ cols = tr.findAll('td')
+ lst = []
+ for td in cols:
+  lst.append(td.get_text().encode("utf-8"))
+ if len(lst) == 22:
+  lst = [x.decode() for x in lst]
+ print(lst)
 yr = list(range(2024,2025))
 frames = []
 for y in yr:
@@ -11,3 +43,4 @@ for y in yr:
 result = pd.concat(frames)
 result.to_csv('synop_42809.csv')
 print('done...........')
+'''
