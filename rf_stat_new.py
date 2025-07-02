f232e4735b2f697069053bd7fe96078cf68a2c72
Dulal Chakrabarti <dulalchakrabarti@gmail.com>
2025-07-02 18:51:42 +0530

    Add files via upload

--- rf_stat_new.py
+++ rf_stat_new.py
@@ -3,18 +3,22 @@ from geopy.geocoders import Nominatim
 import time
 import tabula
 import math
-metsub = {}
-lins = [lns.rstrip('\n') for lns in open('metsub.csv')]
-for lin in lins:
- txt = lin.split(',')
- metsub[txt[2]] = txt[0]
-#print(metsub.keys())
-lines =  [line.rstrip('\n') for line in open('rf.csv')]
-fl = open('dist_lat_lon.csv','w+')
+count=0
+# convert PDF into CSV file
+#tabula.convert_into("rf.pdf", "rf.csv", output_format="csv", pages='all')
+gl = open('dist_lat_lon.csv','w')
+lines = [line.strip('\n') for line in open('rf.csv')]
 for line in lines:
- ln = line.split(',')
- if ln[0].isdigit():
-  if ln[2] not in metsub.keys():
-   print(ln)
-   fl.write(str(ln[0])+','+str(ln[1])+','+str(ln[2])+','+str(ln[3])+','+str(ln[4])+','+str(ln[5])+','+str(ln[6])+','+str(ln[7])+','+str(ln[8])+','+str(ln[9])+','+str(ln[10])+'\n')
-fl.close()
+ line = line.split(',')
+ stn = line[1]
+ per = line[-2][:-1]
+ cat = line[-1]
+ geolocator = Nominatim(user_agent="my_prog", timeout = None)
+ location = geolocator.geocode(stn)
+ if location != None:
+  print(stn,location.latitude, location.longitude,per,cat)
+  gl.write(stn+','+str(location.latitude)+','+str(location.longitude)+','+per+','+cat+'\n')
+  count+=1
+  time.sleep(1.0)
+print(count)
+
